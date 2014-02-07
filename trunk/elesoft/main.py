c0990ca8ecc39218b6c2a54f85584709549c8d70
kocprc@gmail.com <kocprc@gmail.com@fe6f3990-2e05-734b-747b-ae092ae7af76>
2014-02-07 06:26:06 +0000

    cmd system: help exit

--- trunk/elesoft/main.py
+++ trunk/elesoft/main.py
@@ -1,2 +1,29 @@
 #!/usr/bin/python
-print 'HELLO'
+import string
+welcomeinfo = '''NOW YOU HAVE ENTERED
+THE ELEPHANTVS SYSTEM
+USERS INTERFACE
+ENTER 'HELP' FOR HELP'''
+cmdlst=['HELP','EXIT']
+def help():
+	print 'HERE ARE THE COMMANDS AVAILABLE:'
+	for cmd in cmdlst:
+		print cmd+'\t',
+	print '\n',
+def eleinput():
+	tmp = raw_input().upper()
+	return tmp
+	
+# The main part of the system
+print welcomeinfo
+cmdinput = ''
+while cmdinput != 'EXIT':
+	print 'ENTER A COMMAND:'
+	cmdinput = eleinput()
+	if cmdinput == 'HELP':
+		help()
+	else:
+		if not cmdinput in cmdlst:
+			print 'COMMAND NOT FOUND'
+else:
+	print 'EXITING ELEPHANTVS'
